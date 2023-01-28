7b4f3ad73b6a07af25e65d35ebaef0f992a63dd2
xansiety <ferando543@outlook.com>
2023-01-27 20:41:04 -0600

    CRUD Tareas finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -33,13 +33,40 @@ const App = () => {
     setTodos([...todos, todo]);
   };
 
+  const deleteTodo = (id) => {
+    const newTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(newTodos);
+  };
+
+  const updateTodo = (id) => {
+    const newTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.state = !todo.state;
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+  };
+
+  const orderTodos = (todos) => {
+    return todos.sort((a, b) => {
+      if (a.priority === b.priority) return 0;
+      if (a.priority) return -1;
+      if (!a.priority) return 1;
+    });
+  };
+
   return (
     <div className="container mt-2 mb-2">
       <h1 className="my-2 text-center">Tareas por hacer</h1>
       {/* <NoControlados /> */}
       <Formulario addTodo={addTodo} />
       <br></br>
-      <ListaTodos todos={todos} />
+      <ListaTodos
+        todos={orderTodos(todos)}
+        deleteTodo={deleteTodo}
+        updateTodo={updateTodo}
+      />
     </div>
   );
 };
--- src/components/ListaTodos.jsx
+++ src/components/ListaTodos.jsx
@@ -1,13 +1,16 @@
-import React from "react";
+import Todo from "./Todo";
 
-const ListaTodos = ({ todos }) => {
+const ListaTodos = ({ todos, deleteTodo,updateTodo }) => {
   return (
     <div>
       <h2 className="my-2 text-center">Mis tareas por hacer</h2>
-      <ul>
+      <ul className="list-group">
         {todos.map((todo) => (
-          <li key={todo.id}>{todo.title}</li>
+          <Todo key={todo.id} todo={todo} deleteTodo={deleteTodo} updateTodo={updateTodo} />
         ))}
+        {todos.length === 0 && (
+          <li className="list-group-item">No hay tareas por hacer</li>
+        )}
       </ul>
     </div>
   );
