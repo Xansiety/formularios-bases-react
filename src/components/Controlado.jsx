dae59ccb89bb2d1b385f8b1a91a1d794f3eb5bda
xansiety <ferando543@outlook.com>
2023-01-27 19:30:13 -0600

    Formularios controlados, useState

--- src/components/Controlado.jsx
+++ src/components/Controlado.jsx
@@ -8,11 +8,22 @@ const Controlado = () => {
     title: "TODO #01",
     description: "Descripción del TODO #01",
     state: "pendiente",
+    priority: true,
   });
+
+  const { title, description, state, priority } = todo;
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(todo.title, todo.description, todo.state);
+    console.log(title, description, state);
+  };
+
+  const handleChange = (event) => { 
+    const { type, name, value, checked } = event.target;
+    setTodo({
+      ...todo,
+      [name]: type === "checkbox" ? checked : value,
+    });
   };
 
   return (
@@ -22,25 +33,45 @@ const Controlado = () => {
         placeholder="Ingrese TODO"
         className="form-control mb-2"
         name="title"
-        value={todo.title}
+        value={title}
         // onChange={(event) => setTitle(event.target.value)}
-        onChange={(event) => setTodo({ ...todo, title: event.target.value })}
+        // onChange={(event) => setTodo({ ...todo, title: event.target.value })}
+        onChange={handleChange}
       />
       <textarea
         className="form-control mb-2"
         placeholder="Ingrese descripción"
         name="description"
-        value={todo.description}
-        onChange={(event) =>
-          setTodo({ ...todo, description: event.target.value })
-        }
+        value={description}
+        // onChange={(event) =>
+        //   setTodo({ ...todo, description: event.target.value })
+        // }
+        onChange={handleChange}
       />
+
+      <div className="form-check mb-2">
+        <input
+          type="checkbox"
+          name="priority"
+          className="form-check-input"
+          id="inputCheck"
+          checked={priority}
+          //   onChange={(event) =>
+          //     setTodo({ ...todo, priority: event.target.checked })
+          //   }
+          onChange={handleChange}
+        />
+        <label className="form-check-label" htmlFor="inputCheck">
+          Dar Prioridad
+        </label>
+      </div>
 
       <select
         className="form-select mb-2"
         name="state"
-        value={todo.state}
-        onChange={(event) => setTodo({ ...todo, state: event.target.value })}
+        value={state}
+        // onChange={(event) => setTodo({ ...todo, state: event.target.value })}
+        onChange={handleChange}
       >
         <option value="pendiente">Pendiente</option>
         <option value="completado">Completado</option>
